f6c791ad5b10d5937c6bbd3b379da8be581fd5af
Michael Salvatore <michael.salvatore@sgs.com>
2020-04-03 16:30:34 -0400

    Changing Marshal/Unmarshal method names.

--- app/server/src/app/types/nullables.go
+++ app/server/src/app/types/nullables.go
@@ -7,7 +7,7 @@ import (
 
 type NullableString sql.NullString
 
-func (s *NullableString) MarshalJSON() ([]byte, error) {
+func (s *NullableString) Marshal() ([]byte, error) {
 	if s.Valid {
 		return json.Marshal(s.String)
 	} else {
@@ -15,7 +15,7 @@ func (s *NullableString) MarshalJSON() ([]byte, error) {
 	}
 }
 
-func (s *NullableString) UnmarshalJSON(data []byte) error {
+func (s *NullableString) Unmarshal(data []byte) error {
 	var tmp *string
 	if err := json.Unmarshal(data, &tmp); err != nil {
 		return err
